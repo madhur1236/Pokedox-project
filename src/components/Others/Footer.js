70aff9cdd0db999e2f1b3b9e50e90d82bc0cc0d1
madhur1236 <madhurahuja391@yahoo.com>
2022-07-14 14:46:49 +0530

    First commit

---
+++ src/components/Others/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+/*import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";*/
+
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer className="mt-5">
+      <div className="d-flex justify-content-center align-items-center">
+        <p className="mr-1 credits-text">Pokedox</p>
+        
+      </div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
